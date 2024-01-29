fbe1b695168ad81d6510204d150bb424b7a9b916
navsingh3211 <navsingh3211@gmail.com>
2024-01-30 00:06:22 +0530

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get("/non-blocking",async(req,res)=>{
+  res.status(200).send("This page is non-blocking.");
+})
+
+app.get("/blocking",async(req,res)=>{
+  let counter = 0;
+  for(let i = 0;i< 20_000_000_000;i++){
+    counter++;
+  }
+
+  res.status(200).send(`result is ${counter}`);
+});
+
+app.listen(port,()=>{
+  console.log(`app is listening on port ${port}`);
+})
